05122a99c2fad508976472a8171f17c51f1fd695
simon-jo8 <112388673+simon-jo8@users.noreply.github.com>
2025-08-23 22:07:01 +0200

    New route, test file ( not complete )

--- server.go
+++ server.go
@@ -1,23 +1,105 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/status" {
-        http.Error(w, "404 not found.", http.StatusNotFound)
-        return
-    }
+type Response struct {
+	Status string      `json:"status"`
+	Data   interface{} `json:"data"`
+}
 
-	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+type NextYearRequest struct {
+	Year int `json:"year"`
+}
+
+type NextYearResponse struct {
+	Year int `json:"year"`
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	if(r.Method != "GET") {
+		response := Response{
+			Status: "error",
+			Data: map[string]string{
+				"message": "Method not allowed",
+			},
+		}
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+	response := Response{
+		Status: "success",
+		Data: map[string]string{
+			"date": time.Now().Format(time.RFC3339),
+		},
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
+func nextYearHandler(w http.ResponseWriter, r *http.Request) {
+	if(r.Method != "POST") {
+		response := Response{
+			Status: "error",
+			Data: map[string]string{
+				"message": "Method not allowed",
+			},
+		}
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+	// TODO: how to validate the request?
+	request := &NextYearRequest{}
+	err := json.NewDecoder(r.Body).Decode(request) 
+	if err != nil {
+		response := Response{
+			Status: "error",
+			Data: map[string]string{
+				"message": "Invalid request",
+			},
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	response := NextYearResponse{
+		Year: request.Year + 1,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	response := Response{
+		Status: "error",
+		Data: map[string]string{
+			"message": "404 not found",
+		},
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNotFound)
+	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	// List of routes
+	http.HandleFunc("/status", statusHandler)
+	http.HandleFunc("/nextYear", nextYearHandler)
+	http.HandleFunc("/", notFoundHandler)
 
-	fmt.Println("Server is running on http://localhost:8080")
+	println("Server is running on http://localhost:8080")
+	println("Available routes:")
+	println("- GET /status")
+	println("- GET /nextYear")
+	
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
